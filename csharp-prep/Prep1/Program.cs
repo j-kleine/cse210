ba2f3c7ac6d006045d3e84b6b7eb5c05f1a6e29f
j-kleine <joschuadkleine@gmail.com>
2023-04-21 22:04:37 +0200

    corrected variable name l.13

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,6 +10,6 @@ class Program
         Console.Write("What is your last name?");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
