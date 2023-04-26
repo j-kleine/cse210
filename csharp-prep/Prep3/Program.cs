75794b2e805bd75bbf1ca0cb0995949e736c8351
j-kleine <joschuadkleine@gmail.com>
2023-04-27 00:28:40 +0200

    Prep3 core req + stretch1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magNumber = randomGenerator.Next(1, 99);
+
+        // Console.Write("What is the magic number? ");
+        // int magNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+        int guessNumber = 0;
+
+        while (guess != magNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessNumber = guessNumber + 1;
+
+            if (magNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {guessNumber} guesses.");
+            }        }
+
     }
 }
