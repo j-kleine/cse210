758afecb70a277bdcdfef31dde0aeae40e506edf
j-kleine <joschuadkleine@gmail.com>
2023-06-16 01:01:36 +0200

    finished Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue", 4);
+
+        Rectangle rectangle = new Rectangle("Red", 3, 5);
+
+        Circle circle = new Circle("Yellow", 2);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color: {color} - Area: {area}");
+        }
     }
 }
