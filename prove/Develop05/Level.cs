3d6898092e47c73a8d17e10a65a55ff3de58c042
j-kleine <joschuadkleine@gmail.com>
2023-06-26 09:35:28 +0200

    finished scaling of _levelThreshold

--- prove/Develop05/Level.cs
+++ prove/Develop05/Level.cs
@@ -3,11 +3,13 @@ using System;
 public class Level
 {
     private int _levelThreshold;  // Threshold for level-up
+    private int _requiredPoints; // "Next Level in {_requiredPoints}"
     private int _currentLevel;  // Current level
 
     public Level()
     {
-        _levelThreshold = 1100;  // Initial threshold for level 1
+        _levelThreshold = 1000;  // Initial threshold for level 1
+        _requiredPoints = _levelThreshold; // Initial "Next Level in {_requiredPoints}"
         _currentLevel = 1;  // Start at level 1
     }
 
@@ -16,18 +18,26 @@ public class Level
         return _currentLevel;
     }
 
+    public int GetRequiredPoints(int totalPoints)
+    {
+        _requiredPoints = _levelThreshold - totalPoints;
+        return _requiredPoints;
+    }
+
     public void UpdateLevel(int totalPoints)
     {
         if (totalPoints >= _levelThreshold)
         {
             _currentLevel += 1;
-            _levelThreshold = _currentLevel * 1100;
+            float _levelThresholdPoints = (_currentLevel * 1000) * 1.1f;
+            _levelThreshold = (int)_levelThresholdPoints;
         }
     }
 
     public void DisplayLevel(int totalPoints)
     {
         UpdateLevel(totalPoints);
-        Console.WriteLine($"You have {totalPoints} points and are Level {_currentLevel}");
+        GetRequiredPoints(totalPoints);
+        Console.WriteLine($"You have {totalPoints} points and are Level {_currentLevel} ({_requiredPoints} points to reach Level {_currentLevel + 1})");
     }
 }
