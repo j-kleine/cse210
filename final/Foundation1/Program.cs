05eb981c7ba666ba2b78ca42594d1216a7a48215
j-kleine <joschuadkleine@gmail.com>
2023-07-15 08:19:28 +0200

    rewrote code, private attr instead of properties

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,45 +7,40 @@ class Program
         List<Video> videos = new List<Video>();
 
         // Create and add videos
-        Video video1 = new Video
-        {
-            Title = "The Parable of the Slope | Elder Clark G. Gilbert",
-            Author = "The Church of Jesus Christ of Latter-day Saints",
-            Length = 325
-        };
+        Video video1 = new Video("The Parable of the Slope | Elder Clark G. Gilbert", "The Church of Jesus Christ of Latter-day Saints", 325);
         videos.Add(video1);
 
         // Add 3 comments to video 1
-        video1.AddComment(new Comment {CommenterName = "benjamintingey8516", CommentText = "Beautiful message and artistry. Loved this line: 'He will do everything He can to help us turn our slopes toward heaven.'"});
-        video1.AddComment(new Comment {CommenterName = "jcustard", CommentText = "I really love this. Please share talent like this more often. It amplifies Gospel messages in such a striking way! Thank you for making my day."});
-        video1.AddComment(new Comment {CommenterName = "scottsoward", CommentText = "Christ can lift us from wherever we are... What an important message, and so beautifully illustrated." });
+        Comment comment1_1 = new Comment("benjamintingey8516", "Beautiful message and artistry. Loved this line: 'He will do everything He can to help us turn our slopes toward heaven.'");
+        video1.AddComment(comment1_1);
+        Comment comment1_2 = new Comment("jcustard", "I really love this. Please share talent like this more often. It amplifies Gospel messages in such a striking way! Thank you for making my day.");
+        video1.AddComment(comment1_2);
+        Comment comment1_3 = new Comment("scottsoward", "Christ can lift us from wherever we are... What an important message, and so beautifully illustrated.");
+        video1.AddComment(comment1_3);
 
-        Video video2 = new Video
-        {
-            Title = "Angels in the Mirror",
-            Author = "The Church of Jesus Christ of Latter-day Saints",
-            Length = 346
-        };
+        Video video2 = new Video("Angels in the Mirror", "The Church of Jesus Christ of Latter-day Saints", 346);
         videos.Add(video2);
 
         // Add 4 comments to video 2
-        video2.AddComment(new Comment {CommenterName = "mehtafamily2542", CommentText = "When he said that Monica was his wife I smiled real big. I don't know why but it just made me smile. Happy for them."});
-        video2.AddComment(new Comment {CommenterName = "josevivas953", CommentText = "What an incredible and inspiring message. Thank you. Thank you for reminding me of my worth and responsibility of helping others in and outside the church."});
-        video2.AddComment(new Comment {CommenterName = "alishametiam8025", CommentText = "I am inspired by Elder Godoy's story. Indeed, our kindness is needed."});
-        video2.AddComment(new Comment {CommenterName = "joseflores-mz2rl", CommentText = "Beautiful! ♥Thank you for posting this. It touched my heart."});
+        Comment comment2_1 = new Comment("mehtafamily2542", "When he said that Monica was his wife I smiled real big. I don't know why but it just made me smile. Happy for them.");
+        video2.AddComment(comment2_1);
+        Comment comment2_2 = new Comment("josevivas953", "What an incredible and inspiring message. Thank you. Thank you for reminding me of my worth and responsibility of helping others in and outside the church.");
+        video2.AddComment(comment2_2);
+        Comment comment2_3 = new Comment("alishametiam8025", "I am inspired by Elder Godoy's story. Indeed, our kindness is needed.");
+        video2.AddComment(comment2_3);
+        Comment comment2_4 = new Comment("joseflores-mz2rl", "Beautiful! ♥Thank you for posting this. It touched my heart.");
+        video2.AddComment(comment2_4);
 
-        Video video3 = new Video
-        {
-            Title = "The Other Prodigal Son",
-            Author = "The Church of Jesus Christ of Latter-day Saints",
-            Length = 369
-        };
+        Video video3 = new Video("The Other Prodigal Son", "The Church of Jesus Christ of Latter-day Saints", 369);
         videos.Add(video3);
 
         // Add 3 comments to video 3
-        video3.AddComment(new Comment {CommenterName = "shaniaa.1397", CommentText = "'The green-eyed monster of jealousy' This parable teaches on how to overcome it. And love others as Christ loves us ♥"});
-        video3.AddComment(new Comment {CommenterName = "scottcampbell8299", CommentText = "Thank you, Brother Holland, for putting your voice to this wonderful message. Truly a video many can learn from too."});
-        video3.AddComment(new Comment {CommenterName = "shadjohnsen", CommentText = "What a great message to help heal family issues by allowing our hearts to heal!"});
+        Comment comment3_1 = new Comment("shaniaa.1397", "'The green-eyed monster of jealousy' This parable teaches on how to overcome it. And love others as Christ loves us ♥");
+        video3.AddComment(comment3_1);
+        Comment comment3_2 = new Comment("scottcampbell8299", "Thank you, Brother Holland, for putting your voice to this wonderful message. Truly a video many can learn from too.");
+        video3.AddComment(comment3_2);
+        Comment comment3_3 = new Comment("shadjohnsen", "What a great message to help heal family issues by allowing our hearts to heal!");
+        video3.AddComment(comment3_3);
 
         Console.Clear();
         Console.WriteLine("********************************************************************************");
@@ -54,16 +49,16 @@ class Program
         foreach (Video video in videos)
         {
             Console.WriteLine();
-            Console.WriteLine($"Title: <{video.Title}>");
-            Console.WriteLine($"Author: <{video.Author}>");
-            Console.WriteLine($"Length: {video.Length} seconds");
+            Console.WriteLine($"Title: <{video.GetTitle()}>");
+            Console.WriteLine($"Author: <{video.GetAuthor()}>");
+            Console.WriteLine($"Length: {video.GetLength()} seconds");
             Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
 
             Console.WriteLine("Comments:");
             foreach (Comment comment in video.GetComments())
             {
-                Console.WriteLine($"- Commenter: <{comment.CommenterName}>");
-                Console.WriteLine($"- Comment: <{comment.CommentText}>");
+                Console.WriteLine($"- Commenter: <{comment.GetCommenterName()}>");
+                Console.WriteLine($"- Comment: <{comment.GetCommentText()}>");
                 Console.WriteLine();
             }
 
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -2,10 +2,32 @@ using System;
 
 public class Video
 {
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int Length { get; set; }
+    private string _title;
+    private string _author;
+    private int _length;
     private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public string GetAuthor()
+    {
+        return _author;
+    }
+
+    public int GetLength()
+    {
+        return _length;
+    }
 
     public void AddComment(Comment comment)
     {
